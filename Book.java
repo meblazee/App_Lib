9ff7862ac003f2d140c7bd1a0565497306bcace2
blazee <blazej.wietczak@gmail.com>
2018-04-11 22:19:01 +0200

    add this. in book

--- Book.java
+++ Book.java
@@ -6,19 +6,23 @@ class Book {
 	String publisher;
 	String isbn;
 
-	Book(String bookTitle, String bookAuthor, int bookRelease, 
-		    int bookPages, String bookPublisher, String bookIsbn) {
-		title = bookTitle;
-		author = bookAuthor;
-		releaseDate = bookRelease;
-		pages = bookPages;
-		publisher = bookPublisher;
-		isbn = bookIsbn;
+	Book(String title, String author, int releaseDate, 
+		    int pages, String publisher, String isbn) {
+		this.title = title;
+		this.author = author;
+		this.releaseDate = releaseDate;
+		this.pages = pages;
+		this.publisher = publisher;
+		this.isbn = isbn;
 	}
+
+	//Book(Book book) {
+		//this(book.title, book.author, book.releaseDate, book.pages, book.publisher, book.isbn);
+	//}
 
 	void printInfo() {
 		String info = "-------------------- \n" + title + "; \n" + author + "; \n" + releaseDate + "; \n" 
-		            + pages + "; \n" + publisher + "; \n" + isbn;
+		            + pages + "; \n" + publisher + "; \n" + isbn + ";";
 		System.out.println(info);
 	}
 	
