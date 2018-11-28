2785a334612945246730b17de474f120de0bbfed
meblazee <blazej.wietczak@gmail.com>
2018-11-28 12:06:52 +0100

    New main class, loops v0.7

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -4,7 +4,7 @@ import data.Library;
 
 public class LibraryApp {
     public static void main(String[] args) {
-        final String appName = "LibraryApp v0.7(beta)";
+        final String appName = "LibraryApp v0.7";
         System.out.println(appName);
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
